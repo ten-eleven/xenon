9844c791d9f6f8caa6b8ce44d0ea34937958f421
Joseph McElroy <phoey1@gmail.com>
2015-11-17 16:40:20 +0000

    improve example test

--- test/e2e/specs/TestSpec.ts
+++ test/e2e/specs/TestSpec.ts
@@ -54,5 +54,9 @@ describe("example", () => {
     browser.get("http://localhost:3002")
     let chatPage:ChatPage = new ChatPage();
     expect(chatPage.userForm.username.isVisible(5000)).toBe(true);
+    chatPage.userForm.username.type("bob");
+    chatPage.userForm.sendAction.click();
+    expect(chatPage.userForm.isDisplayed()).toBe(false);
+    expect(chatPage.chatForm.isVisible(5000)).toBe(true);
   })
 })
