47d0fdeb1ba31f5acb75667e50052249d5e210fd
Joseph McElroy <phoey1@gmail.com>
2015-11-17 14:33:06 +0000

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
 
-export class TestPage {
-  open() {
-    browser.get("http://localhost:3002/")
-    expect(true).toBe(true)
+export class Component {
+  constructor(parent:Component) {
+
+  }
+}
+
+export class Input extends Component {
+  constructor(parent:Component, selector:Selector) {
+    super(parent);
+  }
+}
+
+export class Button extends Component {
+  constructor(parent:Component, selector:Selector) {
+    super(parent);
+  }
+}
+
+class Selector {
+
+}
+
+export class QASelector extends Selector {
+  constructor(qaSelector:String) {
+  }
+}
+
+export class CSSSelector extends Selector {
+  constructor(qaSelector:String) {
   }
 }
--- test/e2e/specs/TestSpec.ts
+++ test/e2e/specs/TestSpec.ts
@@ -1,8 +1,23 @@
-import {TestPage} from "../../../src/index";
+import {Component, Input, Button, QASelector, CSSSelector} from "../../../src/index";
+
+class ChatForm extends Component {
+
+  message: Input;
+  sendAction: Button;
+  missingAction: Button;
+
+  constructor () {
+    super();
+    this.message = new Input(this, new QASelector("message"))
+    this.sendAction = new Button(this, new CSSSelector("#sendMessage"));
+    this.missingAction = new Button(this, new CSSSelector("#missingAction"));
+  }
+
+}
 
 describe("example", () => {
   it("should pass", () => {
-    var testPage = new TestPage();
-    testPage.open();
+    browser.get("http://localhost:3002")
+    var chatForm:ChatForm = new ChatForm();
   })
 })
