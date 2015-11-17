e2bf5503a59fe09e6689d242c2439e6433e1423e
Ash <ssetem@googlemail.com>
2015-11-17 14:51:52 +0000

    more selector code

--- src/Selectors.ts
+++ src/Selectors.ts
@@ -1,6 +1,25 @@
 
 export class Selector {
+  parent:Selector
+  setParent(parent:Selector) {
+    this.parent = parent
+  }
 
+  getParents(): Selector[]{
+    var parents = this.parent ? this.getParents() : []
+    return parents.concat(this)
+  }
+
+  locatorCSS() {
+    return ""
+  }
+  toLocator() {
+    return By.css(this.locatorCSS())
+  }
+
+  toElement(element:protractor.ElementFinder) {
+    element.element(this.toLocator())
+  }
 }
 
 export class QASelector extends Selector {
