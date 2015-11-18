0c909a388b0a6ed839db27181450b28ca6ab378b
Joseph McElroy <phoey1@gmail.com>
2015-11-18 08:23:17 +0000

    update cssExactText method

--- src/Selectors.ts
+++ src/Selectors.ts
@@ -42,33 +42,36 @@ export class CSSIndexSelector extends Selector {
 export class CSSTextSelector extends Selector {
   constructor(public value:string, public text:string, public exact:boolean) {
     super(value);
-    if (By["cssExactText"] != null) {
-      return;
-    }
+    // if (By["cssExactText"] == null) {
 
-    By.addLocator('cssExactText', (css,text,parent,root) => {
-      let e = parent || document;
-      let elements = e.querySelectorAll(css);
+      By.addLocator('cssExactText', (css,text,parent,root) => {
+        let e = parent || document;
+        let elements = e.querySelectorAll(css);
 
-      Array.prototype.filter.call(elements, (element) => {
-        let textMatch = (element):boolean => {
-          return (element.textContent != null && element.textContent.trim() == text);
-        }
+        Array.prototype.filter.call(elements, (element) => {
+          let textMatch = (element):boolean => {
+            return (element.textContent != null && element.textContent.trim() == text);
+          }
 
-        if (textMatch(element)) {
-          return element;
-        }
-        let children:Array<Object> = element.querySelectorAll("*");
-        for (let childElement of children) {
-          if (textMatch(childElement)) {
+          if (textMatch(element)) {
             return element;
           }
-        }
+          let children:Array<Object> = element.querySelectorAll("*");
+          for (let childElement of children) {
+            if (textMatch(childElement)) {
+              return element;
+            }
+          }
+        })
       })
-    })
+    // }
   }
 
   toElement (element:protractor.ElementFinder) {
-    return element.element(By.cssContainingText(this.value, this.text));
+    if (this.exact) {
+      return element.element(By["cssExactText"](this.value, this.text));
+    } else {
+      return element.element(By.cssContainingText(this.value, this.text));
+    }
   }
 }
