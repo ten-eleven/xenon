ae9098779d7b1b3bf475be74e4a572a19ea9fdfc
Joseph McElroy <phoey1@gmail.com>
2015-11-19 12:20:53 +0000

    remove ts-dest

--- ts-dest/src/components/Index.js
+++
@@ -1,9 +0,0 @@
-var Component_1 = require("./Component");
-exports.Component = Component_1.default;
-var Input_1 = require("./Input");
-exports.Input = Input_1.default;
-var Button_1 = require("./Button");
-exports.Button = Button_1.default;
-var List_1 = require("./List");
-exports.List = List_1.default;
-//# sourceMappingURL=Index.js.map
--- ts-dest/test/unit/specs/TestSpec.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=TestSpec.js.map
